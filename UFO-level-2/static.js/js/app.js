24867ca4fa083ff7273f4c0fd39e0825b98b1a84
Melissa Lowe <mel.c.lowe@gmail.com>
2021-04-10 21:16:03 -0500

    Impression of table present, no data. But also no flagged errors

--- UFO-level-2/static.js/js/app.js
+++ UFO-level-2/static.js/js/app.js
@@ -1,4 +1,12 @@
+console.log("app.js loaded")
+
 // from data.js
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Let's see what a reference to tbody gets me
+var tbody = d3.select("tbody");
+
+// What am I getting?
+console.log(tbody);
--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,23 @@
+console.log("app.js loaded")
+
 // from data.js
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Let's see what a reference to tbody gets me
+var tbody = d3.select("tbody");
+
+// Take a look at the data
+console.log(data);
+
+// Use d3 to append a table row for each sighting
+data.forEach(function(ufoSighting) {
+    // console.log(ufoSighting)
+    var row = tbody.append("tr");
+
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        // console.log(key, value);
+        var cell = row.append("td");
+    })
+});
