6566c25ccb3c683ddbb6b1e285d6f6a7e1c2bd5b
Melissa Lowe <mel.c.lowe@gmail.com>
2021-04-10 22:43:05 -0500

    Button not responding, filter not deploying

--- static/js/app.js
+++ static/js/app.js
@@ -20,5 +20,40 @@ data.forEach(function(ufoSighting) {
         // console.log(key, value);
         var cell = row.append("td");
         cell.text(value);
-    })
+    });
 });
+
+// Notes for filter button: id: datetime
+
+// Assign data to a variable
+var sightings = data;
+
+// Use d.3 to select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create the event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Establish the function runEnter
+function runEnter() {
+
+    // Prevent default reset
+    d3.event.preventDefault();
+
+    // Select input field
+    var inputDate = d3.select("#datetime");
+
+    // Get the value from the input field
+    var dateValue = inputDate.property("value");
+
+    console.log(inputDate);
+    console.log(ufoSighting);
+
+    var filteredSightings = ufoSighting.filter(sighting => sighting.datetime === dateValue);
+
+    console.log(filteredSightings);
+};
